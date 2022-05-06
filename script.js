59729995bfebc65e1f500a25642d2f59db06324e
Jevtovic Aleksa <69771033+jevtovicaleksa@users.noreply.github.com>
2022-05-06 16:03:36 +0200

    Carousel

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ document.querySelector('.hamburger').addEventListener('click', function () {
 document.getElementById('clHamburger').addEventListener('click', function () {
     document.getElementById('closeHamburger').classList.add('hidden');
 })
+
+
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 1500
+    });
+});
